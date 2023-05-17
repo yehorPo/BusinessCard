11ad20e31465c9f30c35c0fef3e7cdba79f8938c
Yehor Podolskyi <66732525+yehorPo@users.noreply.github.com>
2023-05-17 19:07:48 +0300

    init commit

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -9,18 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.cyan)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("frog")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250.0)
+                    .clipShape(Circle())
+                    .overlay(content: {
+                        Circle().stroke(Color.yellow, lineWidth: 5)
+                    })
+                Text("Yehor Podolskyi").font(Font.custom("Ubuntu-Bold", size: 40)).fontWeight(.black).foregroundColor(Color.yellow).bold()
+                Text("Avarage chel")
+                    .foregroundColor(.yellow)
+                    .font(.system(size: 25))
+                    .bold()
+                Divider()
+                roundedRect(text: "1488", imageName: "figure.roll")
+                roundedRect(text: "Мамі привіт", imageName: "sparkles")
+            }
+            
         }
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct roundedRect: View {
+    let text:String
+    let imageName:String
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(.yellow)
+                .frame(height: 50)
+                .cornerRadius(25)
+                .padding(.all)
+            HStack {
+                Image(systemName: imageName)
+                    .font(.system(size: 20))
+                    .foregroundColor(.cyan)
+                Text(text).font(.system(size: 20))
+                    .foregroundColor(.cyan)
+            }
+        }
     }
 }
