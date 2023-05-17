385937d51d7a2271401b2788373e200f9ceb62dc
Yehor Podolskyi <66732525+yehorPo@users.noreply.github.com>
2023-05-17 15:03:31 +0300

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Yehor Podolskyi on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
